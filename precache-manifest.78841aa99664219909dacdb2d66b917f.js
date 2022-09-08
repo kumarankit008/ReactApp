bae61140d777303c620b1b40e8f12e211ac80876
Ankit Kumar <kumarankit.kumar008@gmail.com>
2022-09-08 14:22:39 +0530

    Updates

---
+++ precache-manifest.78841aa99664219909dacdb2d66b917f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "08da1aba79842147b715",
+    "url": "/ReactApp/static/css/main.3a0ff94b.chunk.css"
+  },
+  {
+    "revision": "08da1aba79842147b715",
+    "url": "/ReactApp/static/js/main.8a505873.chunk.js"
+  },
+  {
+    "revision": "8fa6fbb6c0e8399fe15f",
+    "url": "/ReactApp/static/js/runtime~main.eb98a27a.js"
+  },
+  {
+    "revision": "4b944ecdbec3027191e1",
+    "url": "/ReactApp/static/css/2.38df758c.chunk.css"
+  },
+  {
+    "revision": "4b944ecdbec3027191e1",
+    "url": "/ReactApp/static/js/2.8f51ac0a.chunk.js"
+  },
+  {
+    "revision": "cd50bf3c62c66b76d23e13a2c7c8a798",
+    "url": "/ReactApp/index.html"
+  }
+];
